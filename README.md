0fab104dafbba95ab8ffc7f1fe66d467ee9d8fc3
Kyelink <143307261+Kyelink@users.noreply.github.com>
2024-02-04 20:22:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# node-red-contrib-humhub-services
+
+![Platform](https://img.shields.io/badge/platform-Node--RED-red)
+![npm](https://img.shields.io/npm/v/node-red-contrib-custom-chatgpt.svg)
+![Downloads](https://img.shields.io/npm/dt/node-red-contrib-custom-chatgpt.svg)
+![MIT License](https://img.shields.io/badge/license-MIT-blue.svg)
+
+Implement your Node-RED flows with the services proposed by the RESTful API of Humhub.
+
+**Warning: Beta version - Humhub-services will change in the next releases!**
+
+The REST API module provides a generic interface to various subsystems of the HumHub platform.
+
+## Requirements
+
+Ensure you created an Humhub account in order to correctly configure your credentials, either by login/password or with API token/key.
+
+# Creating a post (POST method)
+
+To create a post, you'll need the container's id in which you want to have your post. GET method can get you the container id of a particular post.
+
+# Updating a post (PUT method)
+
+To update a post, you'll need the id of the post you want to change. GET method can get you the id of a particular post.
+
+# Deleting a post (DELETE method)
+
+To delete a post, you'll need the id of the post you want to erase. GET method can get you the id of a particular post.
+
+# Getting one or many post (GET method)
+
+To get all the posts, you can set no filter, if you want to have only some meeting some conditions, you can set as many filters as you need. Filters as only few conditions to examine and compare, they'll be enhanced in subsequent versions.
+
+## Installation
+
+In order to set an API key go to `Administration -> Modules -> Installed -> Rest API` on your Humhub account.
---
+++ humhub-services.js
@@ -0,0 +1,336 @@
+const debug = false;
+
+module.exports = function(RED) {
+
+    function RemoteServerNode(n) {
+        RED.nodes.createNode(this,n);
+        this.username = n.username;
+        this.password = n.password;
+        this.bearer_token = n.bearer_token;
+        this.host = n.host;
+        this.authentification = n.authentification;
+    }
+    RED.nodes.registerType("client-credentials",RemoteServerNode);
+
+
+    function LowerCaseNode(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        this.creds = RED.nodes.getNode(config.creds);
+        this.method = config.method;
+        this.iddtype = config.iddtype;
+        this.idd = config.idd;
+        console.log('\x1b[33m%s\x1b[0m',"method init at :"+ this.method);
+
+        console.log('\x1b[33m%s\x1b[0m',"token init at :"+ this.creds.bearer_token);
+        console.log('\x1b[33m%s\x1b[0m',"auth init at :"+ this.creds.authentification);
+        console.log('\x1b[33m%s\x1b[0m',"host init at :"+ this.creds.host);
+        node.on('input', function(msg) {
+            console.log('\x1b[33m%s\x1b[0m',"method inputed at :"+ this.method);
+            console.log('\x1b[33m%s\x1b[0m',"id inputed at :"+ this.iddtype);
+            console.log('\x1b[33m%s\x1b[0m',"token inputed at :"+ this.creds.bearer_token);
+            
+			if(config.iddtype === "msg"){
+				var buildText = eval("msg." + config.idd)
+				this.idd = buildText;
+            }else if(config.iddtype === "flow"){
+                this.idd = this.context().flow.get(config.idd);
+			}else if(config.iddtype === "global"){
+                this.idd = this.context().global.get(config.idd);
+			}else{
+				this.idd = config.idd;
+			}
+
+
+            var url='https://'+this.creds.host+'/api/v1/post';
+            if(debug){
+                msg.payload=
+                this.method + " : method\n" +
+                this.iddtype + " : id type\n" +
+                this.idd + " : id\n" +
+                this.creds.authentification + " : authentification\n" +
+                this.creds.host + " : host\n" +
+                this.creds.bearer_token + " : token\n" +
+                this.creds.username + " : username\n" +
+                this.creds.password + " : password\n";
+                
+                node.send(msg);
+            }
+            
+            else{
+                let headers = new Headers();
+                if(this.creds.authentification==="Token"){
+                    headers.set('Authorization', 'Bearer ' + this.creds.bearer_token);
+                }
+                else{
+                    headers.set('Authorization', 'Basic ' + btoa(this.creds.username + ":" + this.creds.password));
+                }
+                let meth=this.method;
+                switch (meth) {
+                    case 'DELETE':
+                        url+='/';
+                        url+=this.idd;
+                        var options = {
+                            method : this.method,
+                            headers : headers
+                        };
+                        const promiseJson3= fetch(url,options)
+                        .then((response) => {if (!response.ok){node.error(response.statusText + ", status code : (" + response.status+")");}
+                            return response.json();});
+                        break;
+
+                    case 'GET':
+                        var options = {
+                            method : this.method,
+                            headers : headers
+                        };
+                        const promiseJson2= fetch(url,options)
+                        .then((response) => {if (!response.ok){node.error(response.statusText + ", status code : (" + response.status+")");}
+                            return response.json();});
+                        promiseJson2.then((json) => console.log(json));
+                        promiseJson2.then(function(json){
+                            tempResults=json.results;
+                            this.filters = config.filters;
+                            
+                            for(let i=0;i<this.filters.length;i++){
+                                let fK=this.filters[i].filterKey;
+                                let fO=this.filters[i].filterOperator;
+                                let fV="";
+                                if(this.filters[i].filterValueType === "msg"){
+                                    fV = eval("msg." + this.filters[i].filterValue)
+                                }else if(this.filters[i].filterValueType === "flow"){
+                                    fV = this.context().flow.get(this.filters[i].filterValue);
+                                }else if(this.filters[i].filterValueType === "global"){
+                                    fV= this.context().global.get(this.filters[i].filterValue);
+                                }else{
+                                    fV = this.filters[i].filterValue;
+                                }
+                                switch (fO) {
+                                    case "===":
+                                        tempResults=tempResults.filter(x=>x[fK]==fV);
+                                        break;
+    
+                                    case "!==":
+                                        tempResults=tempResults.filter(x=>x[fK]!=fV);
+                                        break;
+    
+                                    case ">":
+                                        tempResults=tempResults.filter(x=>x[fK]>fV);
+                                        break;
+    
+                                    case "<":
+                                        tempResults=tempResults.filter(x=>x[fK]<fV);
+                                        break;
+    
+                                    case ">=":
+                                        tempResults=tempResults.filter(x=>x[fK]>=fV);
+                                        break;
+    
+                                    case "<=":
+                                        tempResults=tempResults.filter(x=>x[fK]<=fV);
+                                        break;
+                                
+                                    default:
+                                        break;
+                                }
+                                
+    
+                            }
+    
+                            msg.payload=tempResults;
+    
+                        
+ 
+                        console.log("input treated")
+                        node.send(msg);
+
+                        });
+
+                        
+                        break;
+                    case 'PUT':
+                        headers.set('Content-Type', 'application/json; charset=UTF-8');
+                        url+="/";
+                        url+=this.idd;
+                        const putPayload = config.putPayload;
+                        const putPosts=[["message","string"],["scheduled_at","string"],["topics","stringArray"]].map(x=>x[0]);
+                        let bodyPUT = {"data":{
+                            "message":"",
+                            "content":{
+                                "metadata":{
+                                    
+                                },
+                                "topics":[]
+                            }
+                        }};
+                        for(let i=0;i<putPayload.length;i++){
+                            let valueTemp="";
+                            switch(putPayload[i].type){
+                                case "str":
+                                    valueTemp=putPayload[i].value;                                    
+                                    break;
+                                case "msg":
+                                    valueTemp=eval("msg." + putPayload[i].value);
+                                    break
+                                case "flow":
+                                    valueTemp= this.context().flow.get(putPayload[i].value);
+                                    break
+                                case "global":
+                                    valueTemp= this.context().global.get(putPayload[i].value);
+                                    break                         
+                                default:
+                                    break;
+                            }
+                            switch (putPosts[i]) {
+                                
+                                case "message":
+                                    bodyPUT["data"][putPosts[i]]=valueTemp;
+                                    break
+                                case "scheduled_at":
+                                    
+                                    bodyPUT["data"]["content"]["metadata"][putPosts[i]]=valueTemp;
+                                    break
+                                case "topics":
+                                    bodyPUT["data"]["content"][putPosts[i]]=[{"name":valueTemp}];
+                                    break
+                            
+                                default:
+                                    break;
+                            }
+
+                        }
+
+                        options = {
+                            method : this.method,
+                            headers : headers,
+                            body : JSON.stringify(bodyPUT)};
+                        
+                        
+                        const promiseJson4= fetch(url,options)
+                        .then((response) => {if (!response.ok){node.error(response.statusText + ", status code : (" + response.status+")");}
+                            return response.json();});
+                        promiseJson4.then(function(json){console.log(json);
+                        msg.payload=json;
+                        node.send(msg);
+                        });
+
+
+
+                        
+
+
+                        break;
+                        
+                    case 'POST':
+                        url+="/container/";
+                        
+                        const postPosts=[["container id","string"],["message","string"],["scheduled_at","string"],["topics","stringArray"]]
+                        .map(x=>x[0]);
+                        headers.set('Content-Type', 'application/json; charset=UTF-8');
+                        let postPayload = config.postPayload;
+                        let body = {"data":{
+                            "content":{
+                                "metadata":{}
+                            }
+                        }};
+                                              
+                        for(let i=0;i<postPayload.length;i++){
+                            let valueTemp="";
+                            switch(postPayload[i].type){
+                                case "str":
+                                    valueTemp=postPayload[i].value;                                    
+                                    break;
+                                case "msg":
+                                    valueTemp=eval("msg." + postPayload[i].value);
+                                    break
+                                case "flow":
+                                    valueTemp= this.context().flow.get(postPayload[i].value);
+                                    break
+                                case "global":
+                                    valueTemp= this.context().global.get(postPayload[i].value);
+                                    break                         
+                                default:
+                                    break;
+                            }
+                            switch (postPosts[i]) {
+                                case "container id":
+                                    url+=valueTemp;
+                                    
+                                    break;
+                                case "message":
+                                    body["data"][postPosts[i]]=valueTemp;
+                                    break
+                                case "scheduled_at":
+                                    
+                                    body["data"]["content"]["metadata"][postPosts[i]]=valueTemp;
+                                    break
+                                case "topics":
+                                    body["data"]["content"][postPosts[i]]=[{"name":valueTemp}];
+                                    break
+                            
+                                default:
+                                    break;
+                            }
+
+                        }
+
+                        options = {
+                            method : this.method,
+                            headers : headers,
+                            body : JSON.stringify(body)};
+                        const promiseJson= fetch(url,options)
+                        .then((response) => {if (!response.ok){node.error(response.statusText + ", status code : (" + response.status+")");}
+                            return response.json();});
+                        promiseJson.then(function(json){console.log(json);
+                        msg.payload=json;
+                        node.send(msg);
+                    });
+                        
+                        
+                       
+                        
+                        break;
+                
+                    default:
+                        break;
+                }
+
+
+
+                    
+                    
+                    
+                    
+                    // let body = {
+                    //     "data": {
+                    //       "message": this.postPayload[1].value,
+                    //       "content": {
+                    //         "metadata": {
+                    //           "visibility": 1,
+                    //           "state": 1,
+                              
+                    //           "hidden": false,
+                    //           "pinned": false,
+                    //           "locked_comments": false
+                    //         },
+                    //         "topics": [
+                    //          {"name": this.postPayload[3].value}
+                    //         ]
+                    //       }
+                    //     }
+                    //   };
+
+
+             
+                
+                
+               
+                
+            }
+
+        });
+    }
+    RED.nodes.registerType("humhub-services",LowerCaseNode);
+
+    
+}
